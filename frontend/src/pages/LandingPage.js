a9d7733cccf27b113e73da1bf44b014694081fee
adaiuto1 <adaiuto@smu.edu>
2023-04-11 19:04:05 -0500

    remove postview from landingpage

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -12,7 +12,6 @@ function LandingPage(){
     return(<>
         <Page>
         <PageContent>
-        <PostView id={1}></PostView>
           <PageHeader title="Popular Posts" />
           <Grid rows="small" gap="large" pad={{ bottom: "large" }}>
             <PostTemplate title={"I Love Cars"} user={"user1"} date={"Jan 1"} text={"Cars are my favorite"}/>
