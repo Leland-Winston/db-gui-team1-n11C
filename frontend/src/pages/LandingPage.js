eb7801643af0e797429a7d802d0a217c729586b8
MaxwellCalvert <72407676+MaxwellCalvert@users.noreply.github.com>
2023-04-14 15:10:49 -0500

    Update LandingPage.js

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -2,73 +2,78 @@ import React, { useState, useEffect, useContext } from "react";
 import {
   Box,
   Button,
-    Card,
-    CardHeader,
-    Grid,
-    Header,
-    Page,
-    PageContent,
-    PageHeader,
-    Sidebar
-  } from "grommet";
-  import PostTemplate from "../components/PostTemplate";
+  Card,
+  CardHeader,
+  Grid,
+  Header,
+  Page,
+  PageContent,
+  PageHeader,
+  Sidebar,
+} from "grommet";
+import PostTemplate from "../components/PostTemplate";
 import PostView from "./posts/PostView";
 import { getAllGarages, getGaragesByMember } from "../api/garageApi";
 import { useNavigate } from "react-router-dom";
 import UserContext from "../UserContext";
-function LandingPage(){
+function LandingPage() {
   let navigate = useNavigate();
   let currentUser = useContext(UserContext);
   let [garages, setGarages] = useState([]);
-    let [loaded, setLoaded] = useState(false);
-    let [joinedGarages, setJoinedGarages] = useState([]);
-    useEffect(() => {
-      getAllGarages().then(x=>setGarages(x))
-      
-    }, [])
-    return(<>
-    <Grid
-    fill
-    areas={[
-      { name: 'nav', start: [0, 0], end: [0, 0] },
-      { name: 'main', start: [1, 0], end: [1, 0] },
-    ]}
-    columns={['medium', 'flex']}
-    rows={['flex']}
-    gap="small"
-  >
-      <Box gridArea="nav" >
-        <Card pad={{bottom:"medium", horizontal:"medium"}} height="max-content"
-      border="white"
-      margin={{top:"large",
-    left:"large"}}
-      round>
-        <CardHeader>
-        <Header><h4>Browse  Garages</h4></Header>
-        </CardHeader>
-          {garages.map(g=>{
-            return(<>
-            <Button key={g.name} label={g.name}
-            margin="xsmall"
-            onClick={()=>navigate('/garage/' + g.name)}></Button>
-            </>)
-          })}
-        </Card>
-        
-      </Box>
-      <Box gridArea="main">
-      <Page>
-        <PageContent>
-          <PageHeader title="Popular Posts" />
-          <Grid rows="small" gap="large" pad={{ bottom: "large" }}>
-          </Grid>
-        </PageContent>
-        
-      </Page>
-      </Box>
-      
-    </Grid>
-        
-    </>)
+  let [loaded, setLoaded] = useState(false);
+  let [joinedGarages, setJoinedGarages] = useState([]);
+  useEffect(() => {
+    getAllGarages().then((x) => setGarages(x));
+  }, []);
+  return (
+    <>
+      <Grid
+        fill
+        areas={[
+          { name: "nav", start: [0, 0], end: [0, 0] },
+          { name: "main", start: [1, 0], end: [1, 0] },
+        ]}
+        columns={["medium", "flex"]}
+        rows={["flex"]}
+        gap="small"
+      >
+        <Box gridArea="nav">
+          <Card
+            pad={{ bottom: "medium", horizontal: "medium" }}
+            height="max-content"
+            border="white"
+            margin={{ top: "large", left: "large" }}
+            round
+          >
+            <CardHeader>
+              <Header>
+                <h4>Browse Garages</h4>
+              </Header>
+            </CardHeader>
+            {garages.map((g) => {
+              return (
+                <>
+                  <Button
+                    key={g.name}
+                    label={g.name}
+                    margin="xsmall"
+                    onClick={() => navigate("/garage/" + g.name)}
+                  ></Button>
+                </>
+              );
+            })}
+          </Card>
+        </Box>
+        <Box gridArea="main">
+          <Page>
+            <PageContent>
+              <PageHeader title="Popular Posts" />
+              <Grid rows="small" gap="large" pad={{ bottom: "large" }}></Grid>
+            </PageContent>
+          </Page>
+        </Box>
+      </Grid>
+    </>
+  );
 }
 export default LandingPage;
