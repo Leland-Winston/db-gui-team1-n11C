750804e889a81ab171197bfc1e4c072cb04aefc5
Leland Winston <106689000+Leland-Winston@users.noreply.github.com>
2023-04-21 15:43:25 -0500

    dummy data

--- DBGUI.session.sql
+++ DBGUI.session.sql
@@ -6,11 +6,19 @@ CREATE TABLE IF NOT EXISTS users(
   password VARCHAR(255) NOT NULL,
   admin BOOLEAN NOT NULL DEFAULT FALSE
 );
+INSERT INTO users (username, password, admin) VALUES ('johndoe', 'password123', false);
+INSERT INTO users (username, password, admin) VALUES ('bro', 'bro', false);
+INSERT INTO users (username, password, admin) VALUES ('cul', 'cul', false);
+
 CREATE TABLE IF NOT EXISTS profiles(
   profile_id INT NOT NULL,
   bio VARCHAR(1000),
   carma INT DEFAULT 0
 );
+INSERT INTO profiles (profile_id, bio, carma) VALUES (1, 'I like cars', 0);
+INSERT INTO profiles (profile_id, bio, carma) VALUES (2, 'I like cars', 0);
+INSERT INTO profiles (profile_id, bio, carma) VALUES (3, 'I like cars', 10000);
+
 CREATE TABLE IF NOT EXISTS posts(
   post_id INT AUTO_INCREMENT PRIMARY KEY,
   author VARCHAR(255) NOT NULL,
@@ -19,6 +27,10 @@ CREATE TABLE IF NOT EXISTS posts(
   content VARCHAR(2000) NOT NULL,
   rating INT DEFAULT 0
 );
+INSERT INTO posts (author, garage, title, content, rating) VALUES ('johndoe', 'CHEVY', 'title', 'content', 0);
+INSERT INTO posts (author, garage, title, content, rating) VALUES ('bro', 'CHEVY', 'bool', 'bool', 0);
+INSERT INTO posts (author, garage, title, content, rating) VALUES ('cul', 'CHEVY', 'juul', 'juul', 0);
+
 CREATE TABLE IF NOT EXISTS comments(
   comment_id INT AUTO_INCREMENT PRIMARY KEY,
   post_id INT NOT NULL,
@@ -27,6 +39,10 @@ CREATE TABLE IF NOT EXISTS comments(
   content VARCHAR(1000),
   rating INT DEFAULT 0
 );
+INSERT INTO comments (post_id, author, parent, content, rating) VALUES (1, 'johndoe', 0, 'content', 0);
+INSERT INTO comments (post_id, author, parent, content, rating) VALUES (1, 'bro', 0, 'bool', 0);
+INSERT INTO comments (post_id, author, parent, content, rating) VALUES (1, 'cul', 0, 'juul', 0);
+
 CREATE TABLE IF NOT EXISTS garages(
   garage_id INT AUTO_INCREMENT PRIMARY KEY,
   creator VARCHAR(255) NOT NULL,
@@ -34,6 +50,9 @@ CREATE TABLE IF NOT EXISTS garages(
   description VARCHAR(1000) NOT NULL,
   num_posts INT DEFAULT 0
 );
+INSERT INTO garages (creator, name, description, num_posts) VALUES ('johndoe', 'My Garage', 'A place for car enthusiasts to discuss their favorite vehicles', 0);
+INSERT INTO garages (creator, name, description, num_posts) VALUES ('bro', 'Vintage Car Lovers', 'Discuss and admire classic automobiles', 0);
+
 CREATE TABLE IF NOT EXISTS memberships(
   garage_name VARCHAR(255) NOT NULL,
   username VARCHAR(255) NOT NULL
